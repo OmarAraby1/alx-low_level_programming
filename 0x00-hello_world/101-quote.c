ef506fd39044ffd2c9437dafc9f65f7c04e7d60a
Omar Araby <omararaby2001@gmail.com>
2023-03-04 14:50:40 -0800

    8. Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - print a line
  * Return 1 (Succss)
  */
+
 int main(void)
 {
-	fputs("and that piece of art is useful\" -\  Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" -   Dora Korpar, 2015-10-19\n", stdout):
 		return (1);
 }
 

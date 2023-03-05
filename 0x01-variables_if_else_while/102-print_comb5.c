224b08956f9ec106ca7a3398ebed24ee0862b488
Omar Araby <omararaby2001@gmail.com>
2023-03-05 09:40:17 -0800

    combo 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - fun
+ * Return: 0 Siccess
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				for (l = j + 1; l < 10; l++)
+				{
+					if (i != 9 && j != 9)
+					{
+						putchar(l + '0');
+						putchar(k + '0');
+						putchar(' ');
+						putchar(j + '0');
+						putchar(i + '0');
+					}
+					if (i < 9 && j < 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+						

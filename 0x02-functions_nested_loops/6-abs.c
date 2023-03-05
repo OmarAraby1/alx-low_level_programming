1e44063c2219dd2f1316eeb4a2d288f4ba9f04eb
Omar Araby <omararaby2001@gmail.com>
2023-03-05 12:12:40 -0800

    6. Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute
+ * @n: computed
+ * Return: the absolute value
+ */
+
+int _abs(int n)
+{
+	if (n > 1)
+	{
+		return (n);
+	}
+	return (-n);
+}

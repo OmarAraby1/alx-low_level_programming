79ef27ec7fedaa7faf454011607d0db2e0661007
Omar Araby <omararaby2001@gmail.com>
2023-03-05 09:41:08 -0800

    combo 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,11 +19,11 @@ int main(void)
 				{
 					if (i != 9 && j != 9)
 					{
-						putchar(l + '0');
-						putchar(k + '0');
-						putchar(' ');
-						putchar(j + '0');
 						putchar(i + '0');
+						putchar(j + '0');
+						putchar(' ');
+						putchar(k + '0');
+						putchar(l + '0');
 					}
 					if (i < 9 && j < 8)
 					{

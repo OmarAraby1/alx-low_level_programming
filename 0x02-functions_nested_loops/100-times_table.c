89535c853833c8fe6dc6a51d9df86e380e9c684d
Omar Araby <omararaby2001@gmail.com>
2023-03-05 13:22:17 -0800

    100. Times Tble

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,9 +11,9 @@ void print_times_table(int n)
 	{
 		int i, j, k;
 
-		for (i = 0; i < 10; i++)
+		for (i = 0; i <= n; i++)
 		{
-			for(j = 0; j < 10; j++)
+			for(j = 0; j <= n; j++)
 			{
 				k = i * j;
 				

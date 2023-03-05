97625317b42c0ffcebc0270bb69d6b60bcf51520
Omar Araby <omararaby2001@gmail.com>
2023-03-05 07:32:22 -0800

    reverse alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - a function that whatever
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'z'; x >= 'a'; x--)
+	{
+		putcahr(x);
+	}
+	putchar('\n');
+	return (0);
+}

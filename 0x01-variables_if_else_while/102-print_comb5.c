fd0e79f29f24f45598b6da05e240a50d817441c2
Omar Araby <omararaby2001@gmail.com>
2023-03-05 09:53:12 -0800

    combo 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,31 +7,24 @@
 
 int main(void)
 {
-	int i, j, k, l;
+	int num1, num2;
 
-	for (i = 0; i < 10; i++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (j = 0; j < 10; j++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			for (k = 0; k < 10; k++)
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 98 && num2 == 99)
 			{
-				for (l = j + 1; l < 10; l++)
-				{
-					if (i != 9 && j != 9)
-					{
-						putchar(i + '0');
-						putchar(j + '0');
-						putchar(' ');
-						putchar(k + '0');
-						putchar(l + '0');
-					}
-					if (i < 9 && j < 8)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
+				continue;
 			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');

2ddf79d2fda05f1b3feff95a933edc98c8a8b4b1
Omar Araby <omararaby2001@gmail.com>
2023-03-05 13:06:48 -0800

    11. Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,13 +14,13 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			if (i < 98)
+			if (i != 98)
 			{
 				printf("%d, ", i);
 			}
-			else
+			else if (i == 98)
 			{
-				printf("%d", i);
+				printf("%d\n", i);
 			}
 		}
 	}
@@ -28,13 +28,13 @@ void print_to_98(int n)
 	{
 		for (j = n; j >= 98; j--)
 		{
-			if (j > 98)
+			if (j != 98)
 			{
 				printf("%d, ", j);
 			}
-			else
+			else if (i == 98)
 			{
-				printf("%d", j);
+				printf("%d\n", j);
 		
 			}
 		}

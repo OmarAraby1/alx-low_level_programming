7864c150914f2073fc0cd369b18e68a61b7502ed
Omar Araby <omararaby2001@gmail.com>
2023-03-06 11:00:13 -0800

    9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print 1 to 100 and words in case of 3 and 5
+ */
+void main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if ((i % 3 != 0) && (i % 5 != 0))
+		{
+			printf("%d ", i);
+		}
+		else if ((i % 3 == 0) && (i % 5 != 0))
+		{
+			printf("Fizz ");
+		}
+		else if ((i % 3 != 0) && (i % 5 == 0))
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("FizzBuzz ");
+		}
+	}
+}

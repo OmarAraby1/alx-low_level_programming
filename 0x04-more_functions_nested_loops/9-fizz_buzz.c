b23d5b80849a5e2da55cb8f42f133f3ee786fec6
Omar Araby <omararaby2001@gmail.com>
2023-03-06 11:11:02 -0800

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) || (i % 5 == 0))
+		if (i % 3 == 0 || i % 5 == 0)
 		{
 			if (i % 3 == 0)
 			{
@@ -27,9 +27,9 @@ int main(void)
 		}
 		if (i != 100)
 		{
-			printf(" ");
+			_putchar(' ');
 		}
 	}
-	printf("\n")
+	_putchar('\n');
 	return (0);
 }

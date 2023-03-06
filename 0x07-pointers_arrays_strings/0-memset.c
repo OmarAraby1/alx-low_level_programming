eb4bfceaa8d91aeee2c723c9bb140d1f7ff4fad3
Omar Araby <omararaby2001@gmail.com>
2023-03-06 03:02:30 -0800

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,12 +5,13 @@
  * @s: address of memory
  * @b: the value or byte
  * @n: number of bytes to be filled
+ * 
  * Return: changed array using s pointer
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	int i;
 
 	for (i = 0; i < n; i++)
 	{
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,8 +9,8 @@
  * used in the 0x07-pointers_arrays_strings directory
  */
 
-int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);

c3355b7ebce303c9bcef3b6b1ede3996fd1ec3aa
Omar Araby <omararaby2001@gmail.com>
2023-03-06 03:09:46 -0800

    Task 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,16 +1,11 @@
 #ifndef main_h
 #define main_h
-
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
  * Description: Header file containing prototypes for all functions
  * used in the 0x07-pointers_arrays_strings directory
  */
-
-char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
@@ -19,5 +14,4 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
 #endif

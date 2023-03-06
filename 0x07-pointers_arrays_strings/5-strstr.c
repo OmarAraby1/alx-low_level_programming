4455b2c45837ee35ff627c296c5c6e10f9ac045a
Omar Araby <omararaby2001@gmail.com>
2023-03-06 04:38:13 -0800

    Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @needle: substring input
+ * @haystack: reference string
+ * Return: pointer to the begining of the lacted
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *i = haystack;
+		char *p = needle;
+		while (*i == *p && *p != '\0')
+		{
+			i++;
+			p++;
+		}
+		if (*p == '\0')
+		{
+			return (haystack);
+		}
+	}
+	return (0);
+}

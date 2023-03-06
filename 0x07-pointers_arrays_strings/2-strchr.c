ebcc6e2efa587546bc3092244a7d505edd1a283f
Omar Araby <omararaby2001@gmail.com>
2023-03-06 03:48:05 -0800

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: string used
+ * @c: character to be located
+ *
+ * Return: first occurance of c or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (i = 0; i <= (sizeof(s) / sizeof(char)); i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i])
+		}
+		return (0);
+	}
+}

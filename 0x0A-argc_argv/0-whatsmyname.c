2d3db2bf865e114e33e44346086ae5c3b95ac98e
Omar Araby <omararaby2001@gmail.com>
2023-03-10 14:15:05 -0800

    0. It is not what they call you, it is what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * whatsmyname - prints the name of the function
+ * @argc: length of the array
+ * @argv: array of inputs
+ */
+int _whatsmyname(int argc, char **argv)
+{
+	printf("%s\n", *argv);
+}

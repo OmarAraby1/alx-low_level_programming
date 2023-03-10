0b7f8c4cdaee4a304b2d875f16238368f340f050
Omar Araby <omararaby2001@gmail.com>
2023-03-10 14:24:18 -0800

    0. It is not what they call you, it is what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,6 +3,7 @@
  * whatsmyname - prints the name of the function
  * @argc: length of the array
  * @argv: array of inputs
+ * Return: 0 Success
  */
 int main(int argc __attribute__((unused)), char **argv)
 {

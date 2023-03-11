83015e26642a642ef7bc728ec9ce0235cf8723e7
Omar Araby <omararaby2001@gmail.com>
2023-03-11 01:29:01 -0800

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,33 +6,46 @@
  */
 int _atoi(char *s)
 {
-        int i = 0;
-        int n = 0;
-        int signo = 1;
+	int i, d, n, len, f, digit;
 
-        while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
-        {
-                if (s[i] == '-')
-                {
-                        signo *= -1;
-                }
-                i++;
-        }
-        while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
-        {
-                if (n >= 0)
-                {
-                        n = n * 10 - (s[i] - '0');
-                        i++;
-                }
-                else
-                {
-                        n = n * 10 - (s[i] - '0');
-                        i++;
-                }
-        }
-        signo *= -1;
-        return (n * signo);
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+		{
+			++d;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+			{
+				digit = -digit;
+			}
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+			{
+				break;
+			}
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+	{
+		return (0);
+	}
+	return (n);
 }
 /**
  * main - that adds positive numbers
@@ -42,26 +55,27 @@ int _atoi(char *s)
  */
 int main(int argc, char **argv)
 {
-	int i = 1;
-	int sum = 0;
-	int j;
+	int sum, num, i, j, k;
 
-	if (argc == 1)
-	{
-		printf("0\n");
-	}
-	while (i < argc)
+	sum = 0;
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
+			if (argv[i][j] > '9' || argv[i][j] < '0')
 			{
-				printf("Error\n");
+				puts("Error");
 				return (1);
 			}
 		}
-		sum += _atoi(argv[i]);
-		i++;
+	}
+	for (k = 1; k < argc; k++)
+	{
+		num = _atoi(argv[k]);
+		if (num >= 0)
+		{
+			sum += num;
+		}
 	}
 	printf("%d\n", sum);
 	return (0);

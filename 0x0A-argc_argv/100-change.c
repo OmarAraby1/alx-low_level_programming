96fffd106ce422e4d41a01f1b96b61dbcc960e85
Omar Araby <omararaby2001@gmail.com>
2023-03-11 01:51:05 -0800

    5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,54 +1,5 @@
 #include "main.h"
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
- */
-int _atoi(char *s)
-{
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-		{
-			++d;
-		}
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-			{
-				digit = -digit;
-			}
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-			{
-				break;
-			}
-			f = 0;
-		}
-		i++;
-	}
-	if (f == 0)
-	{
-		return (0);
-	}
-	return (n);
-}
-/**
  * main - prints the minimum number of coins to make change
  * @argc: length of inputs
  * @argv: array of inputs
@@ -64,7 +15,7 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		return (1);
 	}
-	x = _atoi(argv[1]);
+	x = atoi(argv[1]);
 	if (x < 0)
 	{
 		printf("0\n");

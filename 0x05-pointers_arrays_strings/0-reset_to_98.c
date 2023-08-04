38025046d75509e5c97ee40f881bf96bbc73751e
Omar Araby <omararaby2001@gmail.com>
2023-08-04 15:43:55 -0700

    DEREFERENCE

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
  * reset_to_98 - change the value we pointin at to 98
  * @n: integer we pointin at
  * Return: false
  */
-
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }

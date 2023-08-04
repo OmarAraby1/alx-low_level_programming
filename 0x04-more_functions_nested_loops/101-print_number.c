b3676993efd881a138d37624a625577680241221
Omar Araby <omararaby2001@gmail.com>
2023-08-04 14:00:40 -0700

    print integer using _putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,42 +1,20 @@
 #include "main.h"
 /**
- * print_number - prints an integer
- * @n: integer printed
- */
+* print_number - Prints a number
+* @n: The number to print
+*
+*/
 void print_number(int n)
 {
-	int i = 0;
-	int j, k, frac;
-	int temp = n;
-	int coef = 10;
-
-	while (temp / 10 != 0)
-	{
-		i += 1;
-		temp = temp / 10;
-	}
+	unsigned int num = n;
 	if (n < 0)
 	{
-		n = n * -1;
-		temp = temp * -1;
 		_putchar('-');
+		num = -num;
 	}
-	_putchar(temp + '0');
-	for (j = i; j > 0; j--)
+	if (num > 9)
 	{
-		if (j == 1)
-		{
-			_putchar(n % 10 + '0');
-		}
-		else
-		{
-			for (k = j; k > 2; k--)
-			{
-				coef = coef * 10;
-			}
-			frac = n / coef;
-			coef = 10;
-			_putchar(frac % 10 + '0');
-		}
+		print_number(num / 10);
 	}
+	_putchar(num % 10 + '0');
 }

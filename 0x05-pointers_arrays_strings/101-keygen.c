9bffd46d4c6b24714f54c5d2df285654ed3f8a85
Omar Araby <omararaby2001@gmail.com>
2023-08-05 13:57:51 -0700

    generate random valid passwords

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,29 +2,33 @@
 #include <time.h>
 
 /**
- * main - generate random valid passwords
- * Return: 0 success
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int pass[100];
 	int i, sum, n;
 
-	sum = 0;
+	sum = 0;	
+
 	srand(time(NULL));
-	for (i = 0; i < 20; i++)
+
+	for (i = 0; i < 100; i++)
 	{
-		pass[i] = rand() % 69;
+		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
-		_putchar(pass[i] + '0');
-		if ((420 - sum) - '0' < 69)
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
 		{
-			n = 420 - sum - '0';
+			n = 2772 - sum - '0';
 			sum += n;
-			_putchar(n + '0');
+			putchar(n + '0');
 			break;
 		}
 	}
+
 	return (0);
 }

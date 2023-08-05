f0fb69f358f54ea83c7701db7d44f3042adf3e3b
Omar Araby <omararaby2001@gmail.com>
2023-08-05 13:39:45 -0700

    print the integer in a string

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,37 +9,39 @@
 int _atoi(char *s)
 {
 	int i, len, isneg, isdig, nth, dig;
-	
+
 	i = 0;
 	len = 0;
 	isneg = 0;
 	isdig = 0;
 	nth = 0;
-	for (; s[i] != '\0'; i++)
+	while (s[len] != '\0')
 	{
-		if (isdig == 0)
+		len++;
+	}
+	while (i < len && isdig == 0)
+	{
+		if (s[i] == '-')
 		{
-			if (s[i] == '-')
-			{
-				isneg = 1;
-				++i;
-			}
-			if (s[i] >= '0' && s[i] <= '9')
-			{
-				dig = s[i] - '0';
-				if (isneg == 1)
-				{
-					dig = -dig;
-				}
-				nth = nth * 10 + dig;
-				isdig = 1;
-				if (s[i + 1] < '0' || s[i + 1] > '9')
-				{
-					break;
-				}
-				isdig = 0;
-			}
+			isneg = 1;
+			++i;
 		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			dig = s[i] - '0';
+			if (isneg == 1)
+			{
+				dig = -dig;
+			}
+			nth = nth * 10 + dig;
+			isdig = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+			{
+				break;
+			}
+			isdig = 0;
+		}
+		i++;
 	}
 	if (isdig == 0)
 	{

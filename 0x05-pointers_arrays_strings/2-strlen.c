96d59c433a8a5016e90d813d33fd20e4dedfddd1
Omar Araby <omararaby2001@gmail.com>
2023-08-05 08:38:24 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - length of string
+ * @s: the string
+ * Return: the length
+ */
+
+int _strlen(char *s)
+{
+	for (int i = 0; s+1!="\0"; i++)
+	{
+	}
+	return(i-1);
+}	

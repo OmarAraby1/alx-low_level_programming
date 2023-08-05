a6c7a034470d89d19875ecbd3f2fe966ac91013c
Omar Araby <omararaby2001@gmail.com>
2023-08-05 14:43:44 -0700

    copy string into another with replacment

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: copy to
+ * @src: copy from
+ * @n: limit
+ * Return: pointer to result
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (j < n && src[j] != '\0' && j < i)
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	return(dest);
+}

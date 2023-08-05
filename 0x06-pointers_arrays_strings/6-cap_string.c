89b93f7e097f066ed6ba5f8eda3c39d7ffca0acb
Omar Araby <omararaby2001@gmail.com>
2023-08-05 15:36:01 -0700

    capitalize words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize all words of string
+ * @s: string input
+ * Return: pointer to resutl
+ */
+
+char *cap_string(char *s)
+{
+	int i, j;
+
+	chap seperators[] = {' ', '\t', '\n' ,',' ,';' ,'.' ,'!' ,'?' ,'"' ,'(' ,')' ,'{' ,'}' };
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] -= 32;
+		}
+		for (j = 0; seperator[j] != '\0'; j++)
+		{
+			if (s[i] == seperator[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
+	}
+	return (s);
+}

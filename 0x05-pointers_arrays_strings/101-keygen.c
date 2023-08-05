22962bf54e56c42883b2136694108006ccd21938
Omar Araby <omararaby2001@gmail.com>
2023-08-05 13:56:50 -0700

    generate random valid passwords

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,19 +8,19 @@
 
 int main(void)
 {
-	int pass[20];
+	int pass[100];
 	int i, sum, n;
 
 	sum = 0;
 	srand(time(NULL));
 	for (i = 0; i < 20; i++)
 	{
-		pass[i] = rand() % 78;
+		pass[i] = rand() % 69;
 		sum += (pass[i] + '0');
 		_putchar(pass[i] + '0');
-		if ((2772 - sum) - '0' < 78)
+		if ((420 - sum) - '0' < 69)
 		{
-			n = 2772 - sum - '0';
+			n = 420 - sum - '0';
 			sum += n;
 			_putchar(n + '0');
 			break;

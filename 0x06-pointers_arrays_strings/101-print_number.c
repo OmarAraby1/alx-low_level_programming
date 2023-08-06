2a79fe80e5e6d7085232675517f9090ab854810e
Omar Araby <omararaby2001@gmail.com>
2023-08-06 07:23:32 -0700

    print integer using putchar

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <math.h>
+/**
+ * print_number - prints an integer.
+ * @n: integer
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	int temp = n;
+	int nth = 0;
+	int i = 0;
+	int dig, ds;
+
+	while (temp > 10)
+	{
+		temp /= 10;
+		nth += 1;
+	}
+	ds = nth;
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	while (i <= ds)
+	{
+		dig = n / pow(10, nth);
+		_putchar(dig + '0');
+		i++;
+		nth--;
+	}
+}

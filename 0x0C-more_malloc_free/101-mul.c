f289620c16dfd9e63c63bbc87463dae62b3817dc
Omar Araby <omararaby2001@gmail.com>
2023-08-06 12:43:26 -0700

    multiply two positive integers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,56 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+
 /**
  * main - multiplies two positive numbers
- * @argc: length of input
- * @argv: array of input
- * Return: no return
- */
-int main(int argc, char **argv)
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
+**/
+
+int main(int argc, char *argv[])
 {
-	char *str1, *str2;
-	int i = 0, len1, len2, *result;
+	unsigned long mul;
+	int i, j;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	str1 = argv[1];
-	str2 = argv[2];
-	while (str1[i])
+
+	for (i = 1; i < argc; i++)
 	{
-		if (str1[i] < '0' || str1[i] > '9')
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			exit(98);
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
 		}
-		len1++;
-		i++;
 	}
-	i = 0;
-	while (str2[i])
-	{
-		if (str2[i] < '0' || str2[i] > '9')
-		{
-			printf("Error\n");
-			exit(98);
-		}
-		len2++;
-		i++;
-	}
-	result = malloc(sizeof(*result) * (len1 + len2 + 1));
-	if (result == NULL)
-	{
-		return (1);
-	}
-	result = atol(argv[1]) * atol(argv[2]);
-	if (result == NULL)
-	{
-		free(result);
-		exit(98);
-	}
+	mul = atol(argv[1]) * atol(argv[2]);
 	printf("%lu\n", mul);
 	return (0);
 }

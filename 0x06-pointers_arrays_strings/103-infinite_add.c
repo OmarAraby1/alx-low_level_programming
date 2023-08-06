e7868618d156a609440a9b8d867e808b27cdaa2b
Omar Araby <omararaby2001@gmail.com>
2023-08-06 11:53:53 -0700

    sum two integers into limited buffer

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,28 +1,24 @@
 #include "main.h"
 /**
- * infinite_add - add 2 strings.
- * @n1: string1.
- * @n2: string2.
+ * infinite_add - add 2 strings
+ * @n1: string1
+ * @n2: string2
  * @r: buffer
  * @size_r: buffer size
- * Return: String with all letters in ROT13 base.
+ * Return: String with all letters in ROT13 base
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int a_len = 0, b_len = 0, carry = 0, a, b, sum, biggest;
-	
+
 	while (n1[a_len] != '\0')
 		a_len++;
 	while (n2[b_len] != '\0')
 		b_len++;
-	if (a_len > b_len)
-		biggest = a_len;
-	else
-		biggest = b_len;
+	biggest = (a_len > b_len) ? a_len : b_len;
 	if ((biggest + 1) >= size_r)
 		return (0);
 	r[biggest + 1] = '\0';
-	
 	while (biggest >= 0)
 	{
 		a = (n1[a_len - 1] - '0');
@@ -35,17 +31,12 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			sum = a + carry;
 		else
 			sum = carry;
-		
 		if (sum > 9)
-		{
 			carry = sum / 10;
 			sum = (sum % 10) + '0';
-		}
 		else
-		{
 			carry = 0;
 			sum = sum + '0';
-		}
 		r[biggest] = sum;
 		a_len--;
 		b_len--;

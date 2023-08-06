3a3db54a99c304df1877bb4c6437edf173eeeccf
Omar Araby <omararaby2001@gmail.com>
2023-08-06 13:07:26 -0700

    persorm mathematical operation on two integers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,1 +1,30 @@
-#include
+#include "3-calc.h"
+
+/**
+ * get_op_func - map tha operation with it's function
+ * @s: the operation
+ * Return: the function
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t op_s[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (op_s[i].op)
+	{
+		if (op_s[i].op == *s)
+		{
+			return (op_s[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,4 +2,29 @@
 #include "3-calc.h"
 /**
  * main - Prints the result of simple operations
- *
+ * it uses get_op_func to get the function from op_func
+ * @argc: number of arguments passed
+ * @argv: list of the arguments
+ * Return: the result
+ */
+int main(int argc, char **argv)
+{
+	int (*oprt)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	oprt = get_op_func(argv[2]);
+
+	if (!oprt)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}

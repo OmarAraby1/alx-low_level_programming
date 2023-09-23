d9b6dd3737c1cbacf6f5e8298a943ca8fd2c16cb
Omar <omararaby2001@gmail.com>
2023-09-23 02:42:49 -0700

    prnt elements of doubly linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: the dlistint_t list to print
+ * Return: the number of elements
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t len = 0;
+
+	while (h != NULL)
+	{
+		fprintf(stdout, "%d\n", h->n);
+		h = h->next;
+		len++;
+	}
+	return (len);
+}

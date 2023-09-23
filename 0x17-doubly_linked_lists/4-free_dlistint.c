dbcb32693d85fe8d569026635a7a92e4cd0ae3ae
Omar <omararaby2001@gmail.com>
2023-09-23 03:08:55 -0700

    free the doubly linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_dlistint - free the elements of the list
+ * @head: the list
+ * Return: nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+	stack_t *aux;
+
+	aux = head;
+	while (head)
+	{
+		aux = head->next;
+		free(head);
+		head = aux;
+	}
+}

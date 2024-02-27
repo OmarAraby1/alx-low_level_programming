97995694a0dc44207f6685ebd9b30fe7b9ad23dd
Omar Araby <omararaby2001@gmail.com>
2024-02-27 13:07:43 -0800

    hash table hash node

--- 0x1A-hash_tables/hash_table.h
+++ 0x1A-hash_tables/hash_table.h
@@ -4,6 +4,20 @@
 #include <unistd.h>
 #include <stdlib.h>
 /**
+ * struct hash_node_s - Node of a hash table
+ *
+ * @key: The key, string
+ * The key is unique in the HashTable
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+/**
  * struct hash_table_s - Hash table data structure
  *
  * @size: The size of the array

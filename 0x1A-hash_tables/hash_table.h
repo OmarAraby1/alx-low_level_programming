b96278282d50594514156d849e83de74766d4773
Omar Araby <omararaby2001@gmail.com>
2024-02-27 13:06:27 -0800

    lets create hash table idk

--- 0x1A-hash_tables/hash_table.h
+++ 0x1A-hash_tables/hash_table.h
@@ -1,10 +1,21 @@
 #ifndef HASH_H
 #define HASH_H
-
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-
+/**
+ * struct hash_table_s - Hash table data structure
+ *
+ * @size: The size of the array
+ * @array: An array of size @size
+ * Each cell of this array is a pointer to the first node of a linked list,
+ * because we want our HashTable to use a Chaining collision handling
+ */
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
